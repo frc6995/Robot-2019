e325c6ebf7ee5082094089ba3daa26abddf1ff54
Joey <42120696+JoeyFabel@users.noreply.github.com>
2019-01-11 17:36:49 -0800

    TalonSRX changed to WPI_TalonSRX & unnecessary imports removed. (#15)
    * TalonSRX changed to WPI_TalonSRX & unnecessary imports removed.
    
    * Using follower mode instead of speed controller groups

--- src/main/java/frc/robot/subsystems/DrivebaseS.java
+++ src/main/java/frc/robot/subsystems/DrivebaseS.java
@@ -2,22 +2,22 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.wpilibj.Preferences;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.Talon;
 
 public class DrivebaseS extends Subsystem {
 
-  private TalonSRX driveLeftFront = null;
-  private TalonSRX driveLeftBack = null;
-  private TalonSRX driveRightFront = null;
-  private TalonSRX driveRightBack = null;
+  private WPI_TalonSRX driveLeftFront = null;
+  private WPI_TalonSRX driveLeftBack = null;
+  private WPI_TalonSRX driveRightFront = null;
+  private WPI_TalonSRX driveRightBack = null;
 
-  
+
   private DifferentialDrive differentialDrive = null;
   private SpeedControllerGroup leftSpeedControllerGroup = null;
   private SpeedControllerGroup rightSpeedControllerGroup = null;
@@ -28,14 +28,16 @@ public class DrivebaseS extends Subsystem {
   }
 
   public DrivebaseS() {
-    driveLeftFront = new TalonSRX(frc.robot.RobotMap.DRIVEBASE_LEFT_TALON_CAN_ID);
-    driveLeftBack = new TalonSRX(frc.robot.RobotMap.DRIVEBASE_LEFTB_TALON_CAN_ID);
-    driveRightFront = new TalonSRX(frc.robot.RobotMap.DRIVEBASE_RIGHT_TALON_CAN_ID);
-    driveRightBack = new TalonSRX(frc.robot.RobotMap.DRIVEBASE_RIGHTB_TALON_CAN_ID);
+    driveLeftFront = new WPI_TalonSRX(frc.robot.RobotMap.DRIVEBASE_LEFT_TALON_CAN_ID);
+    driveLeftBack = new WPI_TalonSRX(frc.robot.RobotMap.DRIVEBASE_LEFTB_TALON_CAN_ID);
+    driveRightFront = new WPI_TalonSRX(frc.robot.RobotMap.DRIVEBASE_RIGHT_TALON_CAN_ID);
+    driveRightBack = new WPI_TalonSRX(frc.robot.RobotMap.DRIVEBASE_RIGHTB_TALON_CAN_ID);
 
-    leftSpeedControllerGroup = new SpeedControllerGroup(driveLeftFront, driveLeftBack);
-    rightSpeedControllerGroup = new SpeedControllerGroup(driveRightFront, driveRightBack);
-    differentialDrive = new DifferentialDrive(leftSpeedControllerGroup, rightSpeedControllerGroup);
+    driveLeftBack.set(ControlMode.Follower, frc.robot.RobotMap.DRIVEBASE_LEFT_TALON_CAN_ID);
+    driveRightBack.set(ControlMode.Follower, frc.robot.RobotMap.DRIVEBASE_RIGHT_TALON_CAN_ID);
+    //leftSpeedControllerGroup = new SpeedControllerGroup(driveLeftFront, driveLeftBack);
+    //rightSpeedControllerGroup = new SpeedControllerGroup(driveRightFront, driveRightBack);
+    differentialDrive = new DifferentialDrive(driveLeftFront, driveRightFront);
  
     driveLeftFront.setNeutralMode(NeutralMode.Brake);
     driveLeftBack.setNeutralMode(NeutralMode.Brake);
