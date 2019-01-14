e141e235ff497c77d4c57ff6f541d45a7c9d90b8
JoeyFabel <joseph.fabel@gmail.com>
2019-01-13 17:22:09 -0800

    Added LadderS Subsystem.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 public class RobotMap {
@@ -14,6 +7,9 @@ public class RobotMap {
   public final static int DRIVEBASE_RIGHT_TALON_CAN_ID = 3;
   public final static int DRIVEBASE_LEFTB_TALON_CAN_ID = 4;
   public final static int DRIVEBASE_RIGHTB_TALON_CAN_ID = 5;
+
+  public final static int LADDER_MOTOR_A_TALON_CAN_ID = 6;
+  public final static int LADDER_MOTOR_B_TALON_CAN_ID = 7;
 
 //OI - joystick
   public final static int OI_JOYSTICK = 0;
---
+++ src/main/java/frc/robot/subsystems/LadderS.java
@@ -0,0 +1,43 @@
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class LadderS extends Subsystem {
+
+  private WPI_TalonSRX ladderMotorA = null;
+  private WPI_TalonSRX ladderMotorB = null;
+  private DifferentialDrive ladderDifferentialDrive = null;
+
+  @Override
+  public void initDefaultCommand() {
+  setDefaultCommand(null);
+  }
+
+  public LadderS() {
+
+    ladderMotorA = new WPI_TalonSRX(frc.robot.RobotMap.LADDER_MOTOR_A_TALON_CAN_ID);  
+    ladderMotorB = new WPI_TalonSRX(RobotMap.LADDER_MOTOR_B_TALON_CAN_ID);
+
+    ladderDifferentialDrive = new DifferentialDrive(ladderMotorA, ladderMotorB);
+  
+
+  }
+
+  public void MoveLadder(double moveSpeed){
+    ladderDifferentialDrive.arcadeDrive(moveSpeed, 0);     //may need to use set.
+  }
+
+  public double GetMotorAEncoderCount() {
+  return ladderMotorA.getSensorCollection().getQuadraturePosition();  
+  }
+
+  public double GetMotorBEncoderCount() {
+  return ladderMotorB.getSensorCollection().getQuadraturePosition();
+  }
+
+
+}
