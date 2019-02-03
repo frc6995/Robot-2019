bb45760957eab2435a996af4f38737e6ed08f917
ReaperCrewGreen <elijah.sauder@gmail.com>
2019-02-02 23:03:37 -0800

    Progress using Map Interface

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -6,14 +6,33 @@ import frc.robot.RobotMap;;
 public class RobotPreferences {
 
     RobotMap rm = new RobotMap();
-    public void buttonsCreate() {
-        int stringLength = 0;
-        stringLength = rm.LIST_OF_COMMANDS.length;
 
-        for(int i=0; i<stringLength; i++) {
-            
-        }
+    public void ButtonsCreate() {
+        SmartDashboard.putString("x", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("y", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("a", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("b", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("dpad_up", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("dpad_down", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("dpad_left", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("dpad_right", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("start", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("back", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("left_bumper", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("right_bumper", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("left_stick_button", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("right_stick_button", RobotMap.LIST_OF_COMMANDS[0]);
+
+        SmartDashboard.putString("left_stick_x", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("left_stick_y", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("right_stick_x", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("right_stick_y", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("left_trigger", RobotMap.LIST_OF_COMMANDS[0]);
+        SmartDashboard.putString("right_trigger", RobotMap.LIST_OF_COMMANDS[0]);
     }
 
+    public void GetCommand() {
+        SmartDashboard.getString(key, defaultValue)
+    }
 
 }
---
+++ src/main/java/frc/robot/controller_map/ControllerMap.java
@@ -0,0 +1,35 @@
+package frc.robot.controller_map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerMap {
+
+    public static String[] LIST_OF_COMMANDS = {
+        "",
+        "AlignTargetC", 
+        "DrievArcadeXbox2C",
+        "DriveArcadeXboxC"
+        
+      };
+      
+      public final Map<String,Integer> xBoxMap;
+      public final Map<String,Object> dashBoardButtons;
+
+      public ControllerMap() {
+        xBoxMap = new HashMap<>();
+        dashBoardButtons = new HashMap<>();
+
+        xBoxMap.put("A", 1);
+        xBoxMap.put("B", 2);
+        xBoxMap.put("X", 3);
+        xBoxMap.put("Y", 4);
+        xBoxMap.put("Left Bumper", 5);
+        xBoxMap.put("Right Bumper", 6);
+        xBoxMap.put("Start", 7);
+        xBoxMap.put("Back", 8);
+        xBoxMap.put("Left Stick Button", 9);
+        xBoxMap.put("Right Stick Button", 10);
+      }
+
+}
