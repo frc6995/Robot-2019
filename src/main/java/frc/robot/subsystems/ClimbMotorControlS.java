b2614aa9e11f2b28793f8674caed7caf9e54e7ea
sammcdo <sam_mcdo@yahoo.com>
2019-02-03 19:24:30 -0800

    started motor subsystem

---
+++ src/main/java/frc/robot/subsystems/ClimbMotorControlS.java
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Spark;
+
+public class ClimbMotorControlS extends Subsystem {
+  public Spark ClimbMotor;
+
+  public
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
