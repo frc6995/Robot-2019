4188007898a437aa5e381bfbba0bd283980d083e
J.A. Shue <j.a.shue@outlook.com>
2019-02-06 13:58:12 -0800

    Committing my changes to OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -5,13 +5,13 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import frc.robot.commands.HatchMechToggleCG;
 
-
 public class OI {
+    public final Joystick stick = new Joystick(RobotMap.OI_JOYSTICK);
+    
     public final XboxController xbox = new XboxController(RobotMap.OI_XBOX);
 
-    public final JoystickButton hatchToggle = new JoystickButton(xbox, RobotMap.BUTTON_HATCH_TOGGLE);
+    public final JoystickButton hatchToggle = new JoystickButton(stick, RobotMap.BUTTON_HATCH_TOGGLE);
 
     public OI() {
         hatchToggle.toggleWhenPressed(new HatchMechToggleCG());
     }
-}
