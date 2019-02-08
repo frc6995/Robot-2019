460c83bfcdcbb317d3294c017b8371685dec74bd
ReaperCrewGreen <elijah.sauder@gmail.com>
2019-02-07 22:56:31 -0800

    removed uneeded imports in the DriveArcadeXbox(2)C files

--- src/main/java/frc/robot/commands/DriveArcadeXbox2C.java
+++ src/main/java/frc/robot/commands/DriveArcadeXbox2C.java
@@ -13,7 +13,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-import frc.robot.RobotMap;
 
 public class DriveArcadeXbox2C extends Command {
   private double forwardBackSpeed = 0;
