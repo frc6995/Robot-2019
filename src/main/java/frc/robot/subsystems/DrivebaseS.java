89638ea2e2e2180122e3afabf5693d98d214bf57
ReaperCrewGreen <elijah.sauder@gmail.com>
2019-02-09 15:52:22 -0800

    removed uneeded imports

--- src/main/java/frc/robot/subsystems/DrivebaseS.java
+++ src/main/java/frc/robot/subsystems/DrivebaseS.java
@@ -1,10 +1,7 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
