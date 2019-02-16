bcd3c76ae27c8cd5353c606fb0efba21e6d15b59
JoeyFabel <joseph.fabel@gmail.com>
2019-02-16 15:06:58 -0800

    Added LadderHomeWithoutLSwitchC.

---
+++ src/main/java/frc/robot/commands/ladder/LadderHomeWithoutLSwitchC.java
@@ -0,0 +1,61 @@
+package frc.robot.commands.ladder;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class LadderHomeWithoutLSwitchC extends Command {
+  private boolean finished = false;
+  private int i = 0;
+  private int j = 0;
+
+  public LadderHomeWithoutLSwitchC() {
+    requires(Robot.m_ladderS);
+    this.setInterruptible(false);
+  }
+
+  @Override
+  protected void initialize() {
+  }
+
+  @Override
+  protected void execute() {
+
+    if (i < 20) {
+      i += 1;
+      Robot.m_ladderS.setLadderPower(-0.2);
+    } else {
+        if (j < 20) {
+          j += 1;
+          Robot.m_ladderS.setLadderPower(0.1);
+        } else {
+          Robot.m_ladderS.resetEncoder();
+          Robot.m_ladderS.setLadderPower(0);
+          finished = true;
+          end();
+        }
+
+
+    }
+
+
+
+
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return finished;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
