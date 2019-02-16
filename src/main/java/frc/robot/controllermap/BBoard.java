006737c26f4e0c9f194f4cdeb6ea30d4b290c71b
ReaperCrewGreen <elijah.sauder@gmail.com>
2019-02-15 23:56:56 -0800

    added the button methods for the button board

--- src/main/java/frc/robot/controllermap/BBoard.java
+++ src/main/java/frc/robot/controllermap/BBoard.java
@@ -2,6 +2,7 @@ package frc.robot.controllermap;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
 
 public class BBoard {
     private static Joystick joystick;
@@ -26,4 +27,123 @@ public class BBoard {
         pinky = new JoystickButton(joystick, BUTTON_5);
     }
 
+    public boolean thumb() {
+        if(thumb.get()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void thumb_runOnPress(Command command) {
+        thumb.whenPressed(command);
+    }
+    public void thumb_runOnRelease(Command command) {
+        thumb.whenReleased(command);
+    }
+    public void thumb_toggleOnPress(Command command) {
+        thumb.toggleWhenPressed(command);
+    }
+    public void thumb_cancelOnPress(Command command) {
+        thumb.cancelWhenPressed(command);
+    }
+    public void thumb_runWhileHeld(Command command) {
+        thumb.whileHeld(command);
+    }
+
+    public boolean index() {
+        if(index.get()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void index_runOnPress(Command command) {
+        index.whenPressed(command);
+    }
+    public void index_runOnRelease(Command command) {
+        index.whenReleased(command);
+    }
+    public void index_toggleOnPress(Command command) {
+        index.toggleWhenPressed(command);
+    }
+    public void index_cancelOnPress(Command command) {
+        index.cancelWhenPressed(command);
+    }
+    public void index_runWhileHeld(Command command) {
+        index.whileHeld(command);
+    }
+
+    public boolean middle() {
+        if(middle.get()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void middle_runOnPress(Command command) {
+        middle.whenPressed(command);
+    }
+    public void middle_runOnRelease(Command command) {
+        middle.whenReleased(command);
+    }
+    public void middle_toggleOnPress(Command command) {
+        middle.toggleWhenPressed(command);
+    }
+    public void middle_cancelOnPress(Command command) {
+        middle.cancelWhenPressed(command);
+    }
+    public void middle_runWhileHeld(Command command) {
+        middle.whileHeld(command);
+    }
+
+    public boolean ring() {
+        if(ring.get()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void ring_runOnPress(Command command) {
+        ring.whenPressed(command);
+    }
+    public void ring_runOnRelease(Command command) {
+        ring.whenReleased(command);
+    }
+    public void ring_toggleOnPress(Command command) {
+        ring.toggleWhenPressed(command);
+    }
+    public void ring_cancelOnPress(Command command) {
+        ring.cancelWhenPressed(command);
+    }
+    public void ring_runWhileHeld(Command command) {
+        ring.whileHeld(command);
+    }
+
+    public boolean pinky() {
+        if(pinky.get()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void pinky_runOnPress(Command command) {
+        pinky.whenPressed(command);
+    }
+    public void pinky_runOnRelease(Command command) {
+        pinky.whenReleased(command);
+    }
+    public void pinky_toggleOnPress(Command command) {
+        pinky.toggleWhenPressed(command);
+    }
+    public void pinky_cancelOnPress(Command command) {
+        pinky.cancelWhenPressed(command);
+    }
+    public void pinky_runWhileHeld(Command command) {
+        pinky.whileHeld(command);
+    }
 }
