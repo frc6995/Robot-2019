06ae9b8e260b63976f8811dc7af3e308f9bd9316
ReaperCrewGreen <elijah.sauder@gmail.com>
2019-02-15 23:31:25 -0800

    finished adding in the methods for the rest of the buttons.

--- src/main/java/frc/robot/controllermap/JStick.java
+++ src/main/java/frc/robot/controllermap/JStick.java
@@ -154,6 +154,22 @@ public class JStick {
         }
     }
 
+    public void button_2_runOnPress(Command command) {
+        two_thumb.whenPressed(command);
+    }
+    public void button_2_runOnRelease(Command command) {
+        two_thumb.whenReleased(command);
+    }
+    public void button_2_toggleOnPress(Command command) {
+        two_thumb.toggleWhenPressed(command);
+    }
+    public void button_2_cancelOnPress(Command command) {
+        two_thumb.cancelWhenPressed(command);
+    }
+    public void button_2_runWhileHeld(Command command) {
+        two_thumb.whileHeld(command);
+    }
+
     public boolean button_3() {
         if(three_topBottomLeft.get()) {
             return true;
@@ -161,6 +177,22 @@ public class JStick {
         else {
             return false;
         }
+    }
+
+    public void button_3_runOnPress(Command command) {
+        three_topBottomLeft.whenPressed(command);
+    }
+    public void button_3_runOnRelease(Command command) {
+        three_topBottomLeft.whenReleased(command);
+    }
+    public void button_3_toggleOnPress(Command command) {
+        three_topBottomLeft.toggleWhenPressed(command);
+    }
+    public void button_3_cancelOnPress(Command command) {
+        three_topBottomLeft.cancelWhenPressed(command);
+    }
+    public void button_3_runWhileHeld(Command command) {
+        three_topBottomLeft.whileHeld(command);
     }
 
     public boolean button_4() {
@@ -172,6 +204,22 @@ public class JStick {
         }
     }
 
+    public void button_4_runOnPress(Command command) {
+        four_topBottomRight.whenPressed(command);
+    }
+    public void button_4_runOnRelease(Command command) {
+        four_topBottomRight.whenReleased(command);
+    }
+    public void button_4_toggleOnPress(Command command) {
+        four_topBottomRight.toggleWhenPressed(command);
+    }
+    public void button_4_cancelOnPress(Command command) {
+        four_topBottomRight.cancelWhenPressed(command);
+    }
+    public void button_4_runWhileHeld(Command command) {
+        four_topBottomRight.whileHeld(command);
+    }
+
     public boolean button_5() {
         if(five_topTopLeft.get()) {
             return true;
@@ -179,6 +227,22 @@ public class JStick {
         else {
             return false;
         }
+    }
+
+    public void button_5_runOnPress(Command command) {
+        five_topTopLeft.whenPressed(command);
+    }
+    public void button_5_runOnRelease(Command command) {
+        five_topTopLeft.whenReleased(command);
+    }
+    public void button_5_toggleOnPress(Command command) {
+        five_topTopLeft.toggleWhenPressed(command);
+    }
+    public void button_5_cancelOnPress(Command command) {
+        five_topTopLeft.cancelWhenPressed(command);
+    }
+    public void button_5_runWhileHeld(Command command) {
+        five_topTopLeft.whileHeld(command);
     }
 
     public boolean button_6() {
@@ -190,6 +254,22 @@ public class JStick {
         }
     }
 
+    public void button_6_runOnPress(Command command) {
+        six_topTopRight.whenPressed(command);
+    }
+    public void button_6_runOnRelease(Command command) {
+        six_topTopRight.whenReleased(command);
+    }
+    public void button_6_toggleOnPress(Command command) {
+        six_topTopRight.toggleWhenPressed(command);
+    }
+    public void button_6_cancelOnPress(Command command) {
+        six_topTopRight.cancelWhenPressed(command);
+    }
+    public void button_6_runWhileHeld(Command command) {
+        six_topTopRight.whileHeld(command);
+    }
+
     public boolean button_7() {
         if(seven_leftTopLeft.get()) {
             return true;
@@ -197,6 +277,22 @@ public class JStick {
         else {
             return false;
         }
+    }
+
+    public void button_7_runOnPress(Command command) {
+        seven_leftTopLeft.whenPressed(command);
+    }
+    public void button_7_runOnRelease(Command command) {
+        seven_leftTopLeft.whenReleased(command);
+    }
+    public void button_7_toggleOnPress(Command command) {
+        seven_leftTopLeft.toggleWhenPressed(command);
+    }
+    public void button_7_cancelOnPress(Command command) {
+        seven_leftTopLeft.cancelWhenPressed(command);
+    }
+    public void button_7_runWhileHeld(Command command) {
+        seven_leftTopLeft.whileHeld(command);
     }
 
     public boolean button_8() {
@@ -208,6 +304,22 @@ public class JStick {
         }
     }
 
+    public void button_8_runOnPress(Command command) {
+        eight_leftTopRight.whenPressed(command);
+    }
+    public void button_8_runOnRelease(Command command) {
+        eight_leftTopRight.whenReleased(command);
+    }
+    public void button_8_toggleOnPress(Command command) {
+        eight_leftTopRight.toggleWhenPressed(command);
+    }
+    public void button_8_cancelOnPress(Command command) {
+        eight_leftTopRight.cancelWhenPressed(command);
+    }
+    public void button_8_runWhileHeld(Command command) {
+        eight_leftTopRight.whileHeld(command);
+    }
+
     public boolean button_9() {
         if(nine_leftMiddleLeft.get()) {
             return true;
@@ -215,6 +327,22 @@ public class JStick {
         else {
             return false;
         }
+    }
+
+    public void button_9_runOnPress(Command command) {
+        nine_leftMiddleLeft.whenPressed(command);
+    }
+    public void button_9_runOnRelease(Command command) {
+        nine_leftMiddleLeft.whenReleased(command);
+    }
+    public void button_9_toggleOnPress(Command command) {
+        nine_leftMiddleLeft.toggleWhenPressed(command);
+    }
+    public void button_9_cancelOnPress(Command command) {
+        nine_leftMiddleLeft.cancelWhenPressed(command);
+    }
+    public void button_9_runWhileHeld(Command command) {
+        nine_leftMiddleLeft.whileHeld(command);
     }
 
     public boolean button_10() {
@@ -226,6 +354,22 @@ public class JStick {
         }
     }
 
+    public void button_10_runOnPress(Command command) {
+        ten_leftMiddleRight.whenPressed(command);
+    }
+    public void button_10_runOnRelease(Command command) {
+        ten_leftMiddleRight.whenReleased(command);
+    }
+    public void button_10_toggleOnPress(Command command) {
+        ten_leftMiddleRight.toggleWhenPressed(command);
+    }
+    public void button_10_cancelOnPress(Command command) {
+        ten_leftMiddleRight.cancelWhenPressed(command);
+    }
+    public void button_10_runWhileHeld(Command command) {
+        ten_leftMiddleRight.whileHeld(command);
+    }
+
     public boolean button_11() {
         if(eleven_leftBottomLeft.get()) {
             return true;
@@ -235,6 +379,22 @@ public class JStick {
         }
     }
 
+    public void button_11_runOnPress(Command command) {
+        eleven_leftBottomLeft.whenPressed(command);
+    }
+    public void button_11_runOnRelease(Command command) {
+        eleven_leftBottomLeft.whenReleased(command);
+    }
+    public void button_11_toggleOnPress(Command command) {
+        eleven_leftBottomLeft.toggleWhenPressed(command);
+    }
+    public void button_11_cancelOnPress(Command command) {
+        eleven_leftBottomLeft.cancelWhenPressed(command);
+    }
+    public void button_11_runWhileHeld(Command command) {
+        eleven_leftBottomLeft.whileHeld(command);
+    }
+
     public boolean button_12() {
         if(twelve_leftBottomRIght.get()) {
             return true;
@@ -242,5 +402,21 @@ public class JStick {
         else {
             return false;
         }
+    }
+
+    public void button_12_runOnPress(Command command) {
+        twelve_leftBottomRIght.whenPressed(command);
+    }
+    public void button_12_runOnRelease(Command command) {
+        twelve_leftBottomRIght.whenReleased(command);
+    }
+    public void button_12_toggleOnPress(Command command) {
+        twelve_leftBottomRIght.toggleWhenPressed(command);
+    }
+    public void button_12_cancelOnPress(Command command) {
+        twelve_leftBottomRIght.cancelWhenPressed(command);
+    }
+    public void button_12_runWhileHeld(Command command) {
+        twelve_leftBottomRIght.whileHeld(command);
     }
 }
