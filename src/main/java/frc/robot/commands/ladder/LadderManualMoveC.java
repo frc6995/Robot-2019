7d9f709f563abc7dee4d6af3a1bdbe6a16bd4d9e
JoeyFabel <joseph.fabel@gmail.com>
2019-02-23 13:17:28 -0800

    Added comments to LadderManualMoveC

--- src/main/java/frc/robot/commands/ladder/LadderManualMoveC.java
+++ src/main/java/frc/robot/commands/ladder/LadderManualMoveC.java
@@ -3,7 +3,7 @@ package frc.robot.commands.ladder;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-//Allows driver to manually move ladder using xbox right stick y axis
+//Allows driver to manually move ladder using joystick buttons 8 & 7.
 
 public class LadderManualMoveC extends Command {
   public LadderManualMoveC() {
@@ -19,7 +19,7 @@ public class LadderManualMoveC extends Command {
     if (Robot.m_oi.stick.button_8() == true) {
       Robot.m_ladderS.setLadderPower(0.5);
     }  else if (Robot.m_oi.stick.button_7() == true) {
-      Robot.m_ladderS.setLadderPower(0.1);
+      Robot.m_ladderS.setLadderPower(0.1); //0.1 will probably have to be changed until the ladder stays in its place.
     }
   }
   @Override
